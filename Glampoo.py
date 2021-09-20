17b5bed2ec82668ed84f5e796d8015e6ed34ebe7
Caivy <samnangkhath5@gmail.com>
2021-09-20 09:08:41 +0700

    the bot running function will now be in Class caivy_comment

--- Glampoo.py
+++ Glampoo.py
@@ -53,28 +53,34 @@ class caivy_comment:
 	def reply_comment(self, data):
 		graph.put_object(data,"comments", message="សូមជួយឆែកសារ 😍")
 
-def main():
-	while True:
-		print("Bot is monitoring comments")
-		time.sleep(1)
-		caivy = caivy_comment()
-		comment_data = graph.get_connections(COMBINED_POST_ID_TO_MONITOR,"comments",order='chronological')
-		# This loop in interation of the data structure 'data''id'
-		for i in comment_data['data']:
-			comment_id = i['id']
-			# The reply id of the variable comment_id
-			reply_data = caivy.monitor_reply_comment(comment_id)
-			# Checking if the reply id match the Page_ID and if so print the statment
-			if reply_data == PAGE_ID:
-				time.sleep(1)
-				print("Comment has already been replies: " + comment_id)
-			# Else it will Reply to the comment_id and private_reply the user that just commented
-			else:
-				caivy.reply_comment(comment_id)
-				print("Reply Comment to " + comment_id)
-				time.sleep(1)
-				caivy.private_reply(comment_id)
+	# A Method for running the bot
+	def bot_on(self):
+		while True:
+			print("Bot is monitoring comments")
+			time.sleep(1)
+			comment_data = graph.get_connections(COMBINED_POST_ID_TO_MONITOR,"comments",order='chronological')
+			# This loop in interation of the data structure 'data''id'
+			for i in comment_data['data']:
+				comment_id = i['id']
+				
+				# The reply id of the variable comment_id
+				reply_data = self.monitor_reply_comment(comment_id)
+				
+				# Checking if the reply id match the Page_ID and if so print the statment
+				if reply_data == PAGE_ID:
+					time.sleep(1)
+					print("Comment has already been replies: " + comment_id)
+				
+				# Else it will Reply to the comment_id and private_reply the user that just commented
+				else:
+					self.reply_comment(comment_id)
+					print("Reply Comment to " + comment_id)
+					time.sleep(1)
+					self.private_reply(comment_id)
 
+def main():
+	caivy = caivy_comment()
+	caivy.bot_on()
 
 
 if __name__ == "__main__":
